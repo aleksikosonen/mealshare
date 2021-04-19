68c0b2c8662e9c04fd994ad86b6daea96378a38d
Niko Lindborg <nikolindborg@Nikos-MacBook-Pro.local>
2021-04-19 23:11:20 +0300

    trying to get the signup form working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const userModel = require('../models/userModel');
-
-const users = userModel.users;
+const {validationResult} = require('express-validator');
 
 const get_user = async(req, res) => {
     console.log('get a user from controller');
@@ -11,6 +10,31 @@ const get_user = async(req, res) => {
     return res.status(200).json(user);
 }
 
+const create_user = async(req, res, next) => {
+    //checking if the request has any validation errors
+    console.log('creating user');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    
+    const user = {};
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.fname = req.body.fname;
+    user.lname = req.body.lname;
+    console.log('userController create_user', user);
+    
+    const id = await userModel.insertUser(user);
+    if(id>0){
+        next();
+    } else{
+        res.status(400).json({error: 'register error'}).end()
+    }
+};
+
 module.exports = {
     get_user,
+    create_user,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,8 @@
 
 const pool = require('../database/db');
 const promisePool = pool.promise();
+const date = (new Date().toLocaleString());
+
 
 const getUser = async(id) => {
     try{
@@ -12,6 +14,21 @@ const getUser = async(id) => {
     }
 }
 
+const insertUser = async (req) => {
+    try{
+        console.log('usermodel insert', req.body)
+        const [rows] = await promisePool.execute(
+            'INSERT INTO ms_user (userid, username, lname, email, fname, password, avatar, bio, admin, vst, vet) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        [req.body.username, req.body.lname, req.body.email, req.body.fname, req.body.password, null, null, null, date ,null]);
+        console.log('usermodel inserts:',rows)
+        return rows.insertId;
+    } catch(e){
+        console.error('userModel from insertUser: ', e);
+        return 0;
+    }
+};
+
 module.exports = {
     getUser,
+    insertUser,
 };
